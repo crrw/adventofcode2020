2fc2487540558434bfdaa247ca926cce820d72fb
aasish23 <47400245+aasish23@users.noreply.github.com>
2020-12-01 10:15:47 -0500

    day 1

---
+++ day1Part1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class day1Part1 {
+    public static void main(String[] args) throws FileNotFoundException{
+        System.out.println(solve());
+    }
+
+    public static long solve() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("input.txt"));
+        List<Integer> list = new ArrayList();
+        
+        
+        while(sc.hasNext()){
+            list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        int start = 0, end = list.size()-1;
+        while(start < end){
+            if(list.get(start) + list.get(end) == 2020) return list.get(start) * list.get(end);
+            else if(list.get(start) + list.get(end) < 2020) start++;
+            else end--;
+        }
+        
+        return -1;
+    }
+}
---
+++ day1Part2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class day1Part2 {
+    public static void main(String[] args) throws FileNotFoundException{
+        System.out.println(solve());
+    }
+
+    public static long solve() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("input2.txt"));
+        List<Long> list = new ArrayList();
+        Collections.sort(list);
+        
+        while(sc.hasNext()){
+            list.add(sc.nextLong());
+        }
+
+        for(int i=0; i<list.size(); i++){
+            for(int j=0; j<list.size(); j++){
+                for(int k=0; k<list.size(); k++){
+                    if(list.get(i) + list.get(j) + list.get(k) == 2020 && i != j && j != k && k != i) return list.get(i) * list.get(j) * list.get(k);
+                }
+            }
+        }
+        return -1;
+    }
+}
